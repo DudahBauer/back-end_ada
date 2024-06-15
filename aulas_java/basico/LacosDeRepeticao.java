c4b873425774d0d741ead3b81fffc83c53e12ff3
8 owie <mevbauer@gmail.com.br>
2024-06-14 23:05:24 -0300

    add arquivos novos :)

---
+++ aulas_java/basico/LacosDeRepeticao.java
@@ -0,0 +1,9 @@
+class LaçosDeRepetição {
+    public static void main(String[] args) {
+     for(int i=1; i<=10; i++)
+        for(int j=1; j<=10; j++)
+     //System.out.println(i);
+     System.out.println(j + "X" + i + "=" + j * i );
+    }
+    
+}
